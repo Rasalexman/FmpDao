61588ee0dcda616f9b61a5cb5badcf1f7375b1d1
Alexander <sphc@yandex.ru>
2021-05-31 23:27:13 +0300

    update example

--- app/src/main/kotlin/pro/krit/fmpdaoexample/MainDatabase.kt
+++ app/src/main/kotlin/pro/krit/fmpdaoexample/MainDatabase.kt
@@ -1,6 +1,12 @@
 package pro.krit.fmpdaoexample
 
+import android.content.Context
+import com.mobrun.plugin.api.HyperHiveState
+import com.rasalexman.kodi.annotations.BindSingle
+import com.rasalexman.sresult.common.extensions.logg
+import pro.krit.generated.database.MainDatabaseImpl
 import pro.krit.hiveprocessor.annotations.FmpDatabase
+import pro.krit.hiveprocessor.provider.HyperHiveConfig
 import pro.krit.hiveprocessor.provider.HyperHiveDatabase
 
 @FmpDatabase
@@ -8,4 +14,24 @@ abstract class MainDatabase : HyperHiveDatabase() {
 
     abstract fun providePmDao(): IPmDataDao
     abstract fun providePmLocalDao(): IPmDataLocalDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: MainDatabase? = null
+
+        fun getDatabase(context: Context, hiperHiveState: HyperHiveConfig): MainDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = MainDatabaseImpl()
+                instance.initialize(HyperHiveState(context), hiperHiveState)
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
