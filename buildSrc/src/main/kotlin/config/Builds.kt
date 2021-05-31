d3bb9f03230940dcc19f03234e18d7f3929f9583
Alexander <sphc@yandex.ru>
2021-06-01 00:27:11 +0300

    update version 1.0.4

--- buildSrc/src/main/kotlin/config/Builds.kt
+++ buildSrc/src/main/kotlin/config/Builds.kt
@@ -22,7 +22,7 @@ object Builds {
     }
 
     object Processor {
-        const val VERSION_CODE = 10003
-        const val VERSION_NAME = "1.0.3"
+        const val VERSION_CODE = 10004
+        const val VERSION_NAME = "1.0.4"
     }
 }
