2c3ee1890802c72385202731257c95da97e962b2
Alexander <sphc@yandex.ru>
2021-05-31 22:51:31 +0300

    release version 1.0.2

---
+++ buildSrc/src/main/kotlin/config/Builds.kt
@@ -0,0 +1,28 @@
+package config
+
+object Builds {
+    const val MIN_VERSION = 21
+    const val COMPILE_VERSION = 30
+    const val TARGET_VERSION = 30
+    const val BUILD_TOOLS = "30.0.2"
+    const val APP_ID = "pro.krit.fmpdaoexample"
+
+    val codeDirs = arrayListOf(
+        "src/main/kotlin"
+    )
+
+    object App {
+        const val VERSION_CODE = 10001
+        const val VERSION_NAME = "1.0.2"
+    }
+
+    object Types {
+        const val DEBUG = "debug"
+        const val RELEASE = "release"
+    }
+
+    object Processor {
+        const val VERSION_CODE = 10001
+        const val VERSION_NAME = "1.0.2"
+    }
+}
---
+++ hiveprocessor/build.gradle.kts
@@ -0,0 +1,58 @@
+import config.Builds
+
+plugins {
+    id("java-library")
+    id("kotlin")
+    id("maven-publish")
+    kotlin("kapt")
+}
+
+sourceSets {
+    getByName("main") {
+        java.setSrcDirs(Builds.codeDirs)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+
+    withJavadocJar()
+    withSourcesJar()
+}
+
+dependencies {
+    api(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
+    implementation(kotlin("stdlib-jdk8", config.Versions.kotlin))
+
+    api(config.Libs.Common.gson)
+
+    implementation("com.squareup:kotlinpoet:1.8.0")
+    implementation("com.google.auto.service:auto-service:1.0")
+    kapt("com.google.auto.service:auto-service:1.0")
+}
+
+group = "pro.krit.hiveprocessor"
+version = Builds.Processor.VERSION_NAME
+
+publishing {
+    publications {
+        create<MavenPublication>("kodigen") {
+            from(components["kotlin"])
+            // You can then customize attributes of the publication as shown below.
+            groupId = "pro.krit.hiveprocessor"
+            artifactId = "hiveprocessor"
+            version = Builds.Processor.VERSION_NAME
+
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "hiveprocessor"
+            url = uri("${buildDir}/publishing-repository")
+        }
+    }
+}
