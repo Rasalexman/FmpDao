9b188b007418cd98ae1cd1ddea47d20a9880868f
Alexander <sphc@yandex.ru>
2021-06-05 19:18:44 +0300

    added restrictions in FmpDaoProcessor.kt and rename `update` to `request` functions names

--- hiveprocessor/src/main/kotlin/pro/krit/hiveprocessor/extensions/FieldExt.kt
+++
@@ -1,7 +0,0 @@
-package pro.krit.hiveprocessor.extensions
-
-import java.lang.reflect.Field
-
-fun Field.isInteger(): Boolean {
-    return this.type.simpleName == "Integer"
-}
--- hiveprocessor/src/main/kotlin/pro/krit/hiveprocessor/extensions/FmpLocalDaoExt.kt
+++ hiveprocessor/src/main/kotlin/pro/krit/hiveprocessor/extensions/FmpLocalDaoExt.kt
@@ -14,26 +14,21 @@
 
 package pro.krit.hiveprocessor.extensions
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-import com.mobrun.plugin.api.request_assistant.PrimaryKey
 import com.mobrun.plugin.models.StatusSelectTable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import pro.krit.hiveprocessor.base.IFmpLocalDao
-import pro.krit.hiveprocessor.common.FieldsBuilder.getFields
-import pro.krit.hiveprocessor.common.LocalDaoFields
+import pro.krit.hiveprocessor.common.FieldsBuilder
 import pro.krit.hiveprocessor.common.QueryBuilder
 import pro.krit.hiveprocessor.common.QueryExecuter.executeStatus
 import pro.krit.hiveprocessor.common.QueryExecuter.executeTransactionStatus
-import java.lang.reflect.Field
-import java.util.*
 
 const val ERROR_CODE_CREATE = 10004
 const val ERROR_CODE_INSERT = 10005
 const val ERROR_CODE_DELETE = 10006
 
 inline fun <reified E : Any, reified S : StatusSelectTable<E>> IFmpLocalDao<E, S>.createTable(): StatusSelectTable<E> {
+    FieldsBuilder.initFields(this, E::class.java.fields)
     val query = QueryBuilder.createTableQuery(this)
     return executeStatus(
         dao = this,
@@ -129,52 +124,4 @@ suspend inline fun <reified E : Any, reified S : StatusSelectTable<E>> IFmpLocal
     notifyAll: Boolean = true
 ): StatusSelectTable<E> {
     return withContext(Dispatchers.IO) { delete(items, notifyAll) }
-}
-
-inline fun <reified E : Any, reified S : StatusSelectTable<E>> IFmpLocalDao<E, S>.initFields() {
-    var localPrimaryKey: Field? = null
-    var localPrimaryKeyName: String? = null
-    val localFields = ArrayList<Field>()
-    val localFieldsNames = mutableMapOf<String, String>()
-    var localFieldsForQuery: String? = null
-
-    val fieldsClass = E::class.java.fields
-    for (field in fieldsClass) {
-        val skipExposeAnnotation = field.getAnnotation(Expose::class.java) != null
-        if (skipExposeAnnotation) {
-            continue
-        }
-
-        val primaryKeyAnnotation = field.getAnnotation(
-            PrimaryKey::class.java
-        )
-        var isPrimary = false
-        if (primaryKeyAnnotation != null) {
-            if (localPrimaryKey != null) {
-                throw UnsupportedOperationException("Not support multiple PrimaryKey")
-            }
-            isPrimary = true
-        }
-        val annotationSerializedName = field.getAnnotation(
-            SerializedName::class.java
-        )
-
-        val name: String = annotationSerializedName?.value ?: field.name
-        if (isPrimary) {
-            localPrimaryKeyName = name
-            localPrimaryKey = field
-        } else {
-            localFields.add(field)
-            localFieldsNames[name] = field.type.simpleName
-        }
-        localFieldsForQuery = getFields(localPrimaryKeyName, localFieldsNames.keys.toList())
-    }
-
-    localDaoFields = LocalDaoFields(
-        fields = localFields,
-        fieldsNamesWithTypes = localFieldsNames,
-        primaryKeyField = localPrimaryKey,
-        primaryKeyName = localPrimaryKeyName,
-        fieldsForQuery = localFieldsForQuery
-    )
 }
